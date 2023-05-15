19ff2792251338208d8698e9e865c534f28baf0f
Dizit3 <dizit3@gmail.com>
2023-05-15 17:42:00 +0300

    FirstCommit

---
+++ Assets/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float mouseSensitivity = 2f;
+
+    private CharacterController characterController;
+    private Camera playerCamera;
+    private float verticalRotation = 0f;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.forward * verticalMovement + transform.right * horizontalMovement;
+        characterController.SimpleMove(movement * movementSpeed);
+
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(Vector3.up, mouseX * mouseSensitivity);
+
+        float mouseY = -Input.GetAxis("Mouse Y");
+        verticalRotation += mouseY * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+}
