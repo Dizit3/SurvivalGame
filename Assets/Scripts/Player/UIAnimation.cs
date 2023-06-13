39e8f79379c57677fd0901243fcac097a31d9306
Dizit3 <dizit3@gmail.com>
2023-06-13 13:34:53 +0300

    1

---
+++ Assets/Scripts/Player/UIAnimation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIAnimation : MonoBehaviour
+{
+    [SerializeField] RectTransform rectTran;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            
+        }
+    }
+}
