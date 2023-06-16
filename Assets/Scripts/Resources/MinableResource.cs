51c732f6b600ccb7c54a703c62e2311870ff4c46
Dizit3 <dizit3@gmail.com>
2023-06-16 22:43:11 +0300

    1

--- Assets/Scripts/Resources/MinableResource.cs
+++ Assets/Scripts/Resources/MinableResource.cs
@@ -2,9 +2,6 @@
 
 public abstract class MinableResource : MonoBehaviour, IDestroyable, IMineable
 {
-    private int amount;
-
-    private int hardness;  
 
     public abstract void Destroy();
     public abstract void Mine(int efficiency);
--- Assets/Scripts/Tools/Pickaxe.cs
+++ Assets/Scripts/Tools/Pickaxe.cs
@@ -1,51 +1,65 @@
-﻿
-using System;
+﻿using DG.Tweening;
 using UnityEngine;
-using DG.Tweening;
 
- public class Pickaxe : Tool
+public class Pickaxe : Tool
 {
     private int durability;
     private int efficiency = 5;
 
+    private Vector3 startPos;
+    private Vector3 targetPos;
 
+
+    private void Awake()
+    {
+        startPos = new Vector3(0.45f, -0.6f, 0.56f);
+
+        targetPos = new Vector3(0.45f - .2f, -0.6f, 0.56f + .3f);
+
+    }
 
     public override GameObject Equip()
     {
         return gameObject;
     }
 
+
+
     public override void Use()
     {
-        RunAnimation();
+
+        var Seq = DOTween.Sequence();
+
+        Seq.Append(transform.DOLocalMove(targetPos, .1f));
+        Seq.Join(transform.DOLocalRotate(new Vector3(0f, -105f, -40f), .1f));
+        Seq.AppendCallback(AnimationEnd);
+        Seq.Append(transform.DOLocalMove(startPos, .2f));
+        Seq.Join(transform.DOLocalRotate(new Vector3(0f, -105f, -13f), .2f));
+
+
     }
 
-    private void RunAnimation()
-    {
-      transform.DOMove(new Vector3())
 
-    }
 
     public void AnimationEnd()
     {
-        if (Input.GetMouseButtonDown(0))
+
+        Debug.Log("TryToUsePickAxe");
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 10f))
         {
-            Debug.Log("TryToUsePickAxe");
-
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 10f))
+            Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.blue, 100f);
+            if (hit.collider.gameObject.TryGetComponent<MinableResource>(out MinableResource minable))
             {
-                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.blue, 100f);
-                if (hit.collider.gameObject.TryGetComponent<MinableResource>(out MinableResource minable))
-                {
-                    Debug.Log("UsePickAxeSuccessful");
-                    minable.Mine(efficiency);
-                }
+                Debug.Log("UsePickAxeSuccessful");
+                minable.Mine(efficiency);
             }
         }
+
     }
 }
 
